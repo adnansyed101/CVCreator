6d875085c2a765702865539aa06e18d127122a32
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-08-09 15:10:13 +0600

    dont know what for

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import PerInp from "./Input/PerInp";
+import EduInp from "./Input/EduInp";
+import JobInp from "./Input/JobInp";
+
 function App() {
-  return <h1>Hello from App</h1>;
+  return (
+    <div className="App">
+      <div className="form">
+        <h1>CV Form</h1>
+        <form>
+          <PerInp />
+          <EduInp />
+          <JobInp />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Input/PerInp.jsx
+++ src/Input/PerInp.jsx
@@ -1,10 +1,43 @@
 const PerInp = () => {
   return (
     <div className="perInf">
-      <input type="text" name="" id="" placeholder="Name" />
-      <input type="text" name="" id="" placeholder="Age" />
-      <input type="email" name="" id="" placeholder="email" />
-      <input type="text" name="" id="" placeholder="Phone" />
+      <fieldset>
+        <legend>Personal Information</legend>
+        <div className="group">
+          <label htmlFor="fName">Name: </label>
+          <input
+            type="text"
+            name=""
+            placeholder="John Doe"
+            className="input"
+            id="fName"
+          />
+          <label htmlFor="age">Age: </label>
+          <input
+            type="text"
+            name=""
+            placeholder="18"
+            className="input"
+            id="age"
+          />
+        </div>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          name=""
+          placeholder="something@something.com"
+          className="input"
+          id="email"
+        />
+        <label htmlFor="phone">Phone: </label>
+        <input
+          type="text"
+          name=""
+          placeholder="0172******"
+          className="input"
+          id="phone"
+        />
+      </fieldset>
     </div>
   );
 };
