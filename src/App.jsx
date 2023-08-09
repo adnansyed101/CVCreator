9e25b7f2295596f4c3e988b5f0bd9b816322fa47
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-08-09 20:08:33 +0600

    add personal info state and a function to update
    this state and function will keep track of the personal information.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import PerInp from "./Input/PerInp";
 import EduInp from "./Input/EduInp";
 import JobInp from "./Input/JobInp";
+import { useState } from "react";
 
 function App() {
+  const [personal, setPersonal] = useState({
+    personalName: "",
+    age: "",
+    email: "",
+    phone: "",
+  });
+
+  const handlePersonalChange = (e) => {
+    setPersonal((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
   return (
     <div className="App">
       <div className="form">
         <h1>CV Form</h1>
         <form>
-          <PerInp />
+          <PerInp handleChange={handlePersonalChange} />
           <EduInp />
           <JobInp />
         </form>
