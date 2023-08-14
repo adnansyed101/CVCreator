f563b0e905c52390cc2dc88f206552987e980045
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-08-14 22:48:50 +0600

    add markup for eduinp.

--- src/Input/EduInp.jsx
+++ src/Input/EduInp.jsx
@@ -1,8 +1,63 @@
-
 const EduInp = () => {
   return (
-    <div>EduInp</div>
-  )
-}
+    <div className="eduInf">
+      <fieldset>
+        <legend>Educational Information</legend>
+        <div className="group">
+          <p className="control">
+            <label htmlFor="eduStart">Start</label>
+            <input
+              type="date"
+              // name="personalName"
+              // placeholder="John Doe"
+              className="input"
+              id="eduStart"
+              // onChange={handleChange}
+            />
+          </p>
+          <p className="control">
+            <label htmlFor="age">Institute</label>
+            <input
+              type="text"
+              name="eduInstitute"
+              placeholder="BRAC University"
+              className="input"
+              id="age"
+              // onChange={handleChange}
+            />
+          </p>
+        </div>
+        <div className="group">
+          <p className="control">
+            <label htmlFor="eduEnd">End</label>
+            <input
+              type="date"
+              // name="personalName"
+              // placeholder="John Doe"
+              className="input"
+              id="eduStart"
+              // onChange={handleChange}
+            />
+          </p>
+          <p className="control">
+            <label htmlFor="age">Title</label>
+            <input
+              type="text"
+              name="eduTitle"
+              placeholder="BBA"
+              className="input"
+              id="age"
+              // onChange={handleChange}
+            />
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="eduDesc">Describe</label>
+          <textarea name="eduDesc" id="EduDesc"  />
+        </p>
+      </fieldset>
+    </div>
+  );
+};
 
-export default EduInp
+export default EduInp;
