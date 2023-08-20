5a197c09e5d37356b8fea81d6f2b1aefe53601e0
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-08-20 22:03:06 +0600

    Update EduInp.jsx
    Now mapping over the education state. also dynamically rendering the buttons on need.

--- src/Input/EduInp.jsx
+++ src/Input/EduInp.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable react/prop-types */
 const EduInp = ({ education, setEducation }) => {
-  
   const handleChange = (e, index) => {
     const { name, value } = e.target;
     const list = [...education];
@@ -7,20 +7,20 @@ const EduInp = ({ education, setEducation }) => {
     setEducation(list);
   };
 
-  const handleAddEducation = () => {
+  const addEducation = () => {
     setEducation([
       ...education,
       {
-        institute: "",
-        degreeName: "",
-        startDate: "",
-        endDate: "",
-        description: "",
+        eduStart: "",
+        eduInstitute: "",
+        eduEnd: "",
+        eduTitle: "",
+        eduDesc: "",
       },
     ]);
   };
 
-  const handleEducationRemove = (index) => {
+  const removeEducation = (index) => {
     const list = [...education];
     list.splice(index, 1);
     setEducation(list);
@@ -30,67 +30,87 @@ const EduInp = ({ education, setEducation }) => {
     <div className="eduInf">
       <fieldset>
         <legend>Educational Information</legend>
-        <div className="group">
-          <p className="control">
-            <label htmlFor="eduStart">Start</label>
-            <input
-              type="date"
-              name="eduStart"
-              className="input"
-              id="eduStart"
-              onChange={handleChange}
-            />
-          </p>
-          <p className="control">
-            <label htmlFor="age">Institute</label>
-            <input
-              type="text"
-              name="eduInstitute"
-              placeholder="BRAC University"
-              className="input"
-              id="age"
-              onChange={handleChange}
-            />
-          </p>
-        </div>
-        <div className="group">
-          <p className="control">
-            <label htmlFor="eduEnd">End</label>
-            <input
-              type="date"
-              name="eduEnd"
-              className="input"
-              id="eduStart"
-              onChange={handleChange}
-            />
-          </p>
-          <p className="control">
-            <label htmlFor="eduTitle">Title</label>
-            <input
-              type="text"
-              name="eduTitle"
-              placeholder="BBA"
-              className="input"
-              id="eduTitle"
-              onChange={handleChange}
-            />
-          </p>
-        </div>
-        <p className="control">
-          <label htmlFor="eduDesc">Describe</label>
-          <textarea
-            name="eduDesc"
-            id="EduDesc"
-            placeholder="Give description of course."
-          />
-        </p>
-        <button type="button" className="addBtn">
-          Add Education
-        </button>
-        <button type="button" className="removeBtn">
-          Remove Education
-        </button>
-        <hr />
+        {education.map((data, index) => {
+          return (
+            <div key={index}>
+              <div className="group">
+                <p className="control">
+                  <label htmlFor="eduStart">Start</label>
+                  <input
+                    type="date"
+                    name="eduStart"
+                    className="input"
+                    id="eduStart"
+                    onChange={(e) => handleChange(e, index)}
+                    value={data.eduStart}
+                  />
+                </p>
+                <p className="control">
+                  <label htmlFor="eduInstitute">Institute</label>
+                  <input
+                    type="text"
+                    name="eduInstitute"
+                    placeholder="BRAC University"
+                    className="input"
+                    id="eduInstitute"
+                    onChange={(e) => handleChange(e, index)}
+                    value={data.eduInstitute}
+                  />
+                </p>
+              </div>
+              <div className="group">
+                <p className="control">
+                  <label htmlFor="eduEnd">End</label>
+                  <input
+                    type="date"
+                    name="eduEnd"
+                    className="input"
+                    id="eduEnd"
+                    onChange={(e) => handleChange(e, index)}
+                    value={data.eduEnd}
+                  />
+                </p>
+                <p className="control">
+                  <label htmlFor="eduTitle">Title</label>
+                  <input
+                    type="text"
+                    name="eduTitle"
+                    placeholder="BBA"
+                    className="input"
+                    id="eduTitle"
+                    onChange={(e) => handleChange(e, index)}
+                    value={data.eduTitle}
+                  />
+                </p>
+              </div>
+              <p className="control">
+                <label htmlFor="eduDesc">Describe</label>
+                <textarea
+                  name="eduDesc"
+                  id="EduDesc"
+                  placeholder="Give description of course."
+                  onChange={(e) => handleChange(e, index)}
+                  value={data.eduDesc}
+                />
+              </p>
+              {education.length - 1 === index && (
+                <button type="button" className="addBtn" onClick={addEducation}>
+                  Add Education
+                </button>
+              )}
+              {education.length !== 1 && (
+                <button
+                  type="button"
+                  className="removeBtn"
+                  onClick={() => removeEducation(index)}
+                >
+                  Remove Education
+                </button>
+              )}
+              <hr />
+            </div>
+          );
+        })}
       </fieldset>
     </div>
   );
