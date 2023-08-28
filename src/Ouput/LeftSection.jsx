72308a6675d6ce48994edef65724b8aa6428aa9b
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-08-29 01:53:40 +0600

    remove header part
    removing the header portions and putting it into the header component.

--- src/Ouput/LeftSection.jsx
+++ src/Ouput/LeftSection.jsx
@@ -1,13 +1,5 @@
-/* eslint-disable react/prop-types */
-const LeftSection = ({ personalName, address, email, phone, title }) => {
-  return (
-    <div className="leftSection">
-      <div className="header">
-        <h1>{personalName ? personalName : "John Doe"}</h1>
-        <h2>{title ? title : "Senior Developer"}</h2>
-      </div>
-    </div>
-  );
+const LeftSection = () => {
+  return <div>LeftSection</div>;
 };
 
 export default LeftSection;
--- src/Ouput/RightSection.jsx
+++ src/Ouput/RightSection.jsx
@@ -1,12 +1,5 @@
-/* eslint-disable react/prop-types */
-const RightSection = ({ personImg }) => {
-  return (
-    <div className="rightSection">
-      <figure className="imgDiv">
-        <img src={personImg} alt="Avatar" />
-      </figure>
-    </div>
-  );
+const RightSection = () => {
+  return <div>RightSection</div>;
 };
 
 export default RightSection;
