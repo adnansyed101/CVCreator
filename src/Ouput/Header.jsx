bb0a11e64554f02ff920c538cc303586957e9cc3
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-08-29 01:52:33 +0600

    add markup for header

---
+++ src/Ouput/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/placeholder.jpeg" alt="Avatar" />
+      <div className="info">
+        <h1>MD ADNAN</h1>
+        <h2>JUNIOR WEB DEVELOPER</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
