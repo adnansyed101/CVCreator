cd53dacd16d45264b2bb51c40b41b00464ed55cd
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-09-04 17:37:35 +0600

    add markup for right section

--- src/Ouput/RightSection.jsx
+++ src/Ouput/RightSection.jsx
@@ -1,5 +1,37 @@
+import { FaLocationArrow } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { BsFillPhoneFill } from "react-icons/bs";
+
 const RightSection = () => {
-  return <div>RightSection</div>;
+  return (
+    <div className="rightSection">
+      <h1>Contact Me</h1>
+      <div className="rightSubInfo">
+        <FaLocationArrow style={{ fontSize: "2rem", color: "#ff9d01" }} />
+        <div className="rightInfo">
+          <h4>Address</h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi,
+            quae!
+          </p>
+        </div>
+      </div>
+      <div className="rightSubInfo">
+        <MdEmail style={{ fontSize: "2rem", color: "#ff9d01" }} />
+        <div className="rightInfo">
+          <h4>Email</h4>
+          <p>something@something.com</p>
+        </div>
+      </div>
+      <div className="rightSubInfo">
+        <BsFillPhoneFill style={{ fontSize: "2rem", color: "#ff9d01" }} />
+        <div className="rightInfo">
+          <h4>Phone</h4>
+          <p>01725290448</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightSection;
