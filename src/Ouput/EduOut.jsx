1ac179db3b4d8eb42f3861aa30c920ec1c3d4626
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-09-04 13:13:40 +0600

    created job and edu

--- src/Ouput/EduOut.jsx
+++ src/Ouput/EduOut.jsx
@@ -1,5 +1,20 @@
-const EduOut = () => {
-  return <div>EduOut</div>;
+/* eslint-disable react/prop-types */
+const EduOut = ({ eduStart, eduInstitute, eduEnd, eduTitle, eduDesc }) => {
+  return (
+    <div className="out">
+      <div className="block"></div>
+      <div className="insYear">
+        <span>
+          {new Date(eduStart).getFullYear()} - {new Date(eduEnd).getFullYear()}
+        </span>
+        <p>{eduInstitute ? eduInstitute : "ABC Institute"}</p>
+      </div>
+      <div className="titleDesc">
+        <h4>{eduTitle ? eduTitle : "CSE"}</h4>
+        <p>{eduDesc ? eduDesc : "This description"}</p>
+      </div>
+    </div>
+  );
 };
 
 export default EduOut;
--- src/Ouput/JobOut.jsx
+++ src/Ouput/JobOut.jsx
@@ -1,7 +1,20 @@
-const JobOut = () => {
+/* eslint-disable react/prop-types */
+const JobOut = ({ jobStart, jobInstitute, jobEnd, jobTitle, jobDesc }) => {
   return (
-    <div>JobOut</div>
-  )
-}
+    <div className="out">
+      <div className="block"></div>
+      <div className="insYear">
+        <span>
+          {new Date(jobStart).getFullYear()} - {new Date(jobEnd).getFullYear()}
+        </span>
+        <p>{jobInstitute ? jobInstitute : "ABC Institute"}</p>
+      </div>
+      <div className="titleDesc">
+        <h4>{jobTitle ? jobTitle : "Senior Developer"}</h4>
+        <p>{jobDesc ? jobDesc : "This description"}</p>
+      </div>
+    </div>
+  );
+};
 
-export default JobOut
+export default JobOut;
