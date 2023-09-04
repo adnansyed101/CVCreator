9242078137280229820448e7c714ad18834f49d9
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-09-04 13:13:10 +0600

    Add elements to left section of CV

--- src/Ouput/LeftSection.jsx
+++ src/Ouput/LeftSection.jsx
@@ -1,5 +1,24 @@
-const LeftSection = () => {
-  return <div>LeftSection</div>;
+/* eslint-disable react/prop-types */
+import EduOut from "./EduOut";
+import JobOut from "./JobOut";
+
+const LeftSection = ({ education, job }) => {
+  const eduOutElement = education.map((item, index) => {
+    return <EduOut {...item} key={index} />;
+  });
+
+  const jobOutElement = job.map((item, index) => {
+    return <JobOut {...item} key={index} />;
+  });
+
+  return (
+    <div className="leftSection">
+      <div className="leftHeader">EDUCATION</div>
+      <div style={{ marginBottom: "5rem" }}>{eduOutElement}</div>
+      <div className="leftHeader">Experience</div>
+      <div style={{ marginBottom: "5rem" }}>{jobOutElement}</div>
+    </div>
+  );
 };
 
 export default LeftSection;
